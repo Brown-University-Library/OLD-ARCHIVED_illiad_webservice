98d80bd944b0ab3e660c277641da468f95d0bd8d
birkin <birkin.diana@gmail.com>
2015-10-23 11:01:13 -0400

    checks params.

--- illiad_app/models.py
+++ illiad_app/models.py
@@ -19,8 +19,26 @@ class V2_Helper( object ):
         self.place_holder = 'foo'
 
     def check_validity( self, request ):
-        """ Checks post and auth_key and ip.
+        """ Checks post, auth_key, ip, & params.
             Called by make_request_v2() """
+        log.debug( 'starting check_validity()' )
         return_val = False
+        if request.method is 'POST' or project_settings.DEBUG is True:
+            return_val = True
+        log.debug( 'return_val, `%s`' % return_val )
+        return return_val
+
+    def check_params( self, request ):
+        """ Checks params.
+            Called by check_validity() """
+        log.debug( 'starting check_params()' )
+        return_val = None
+        required_params = [ 'auth_key', 'openurl', 'request_id', 'username' ]
+        for param in required_params:
+            if param is not in request.POST.keys():
+                return_val = False
+                break
+        if return_val is None:
+            return_val = True
         log.debug( 'return_val, `%s`' % return_val )
         return return_val
