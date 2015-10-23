1d767e00f704183f6c708d6f74bed1439256d498
birkin <birkin.diana@gmail.com>
2015-10-23 14:56:10 -0400

    checks for required param-keys.

--- illiad_app/models.py
+++ illiad_app/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import datetime, json, logging, os, pprint, itertools
-from django.conf import settings as project_settings
-from django.core.urlresolvers import reverse
-from django.db import models
-from django.http import HttpResponseRedirect
-from django.utils.encoding import smart_unicode
-from django.utils.text import slugify
+import datetime, json, logging, os, pprint
+# from django.conf import settings as project_settings
+# from django.core.urlresolvers import reverse
+# from django.db import models
+# from django.http import HttpResponseRedirect
+# from django.utils.encoding import smart_unicode
+# from django.utils.text import slugify
 
 log = logging.getLogger(__name__)
 
@@ -23,8 +23,10 @@ class V2_Helper( object ):
             Called by make_request_v2() """
         log.debug( 'starting check_validity()' )
         return_val = False
-        if request.method is 'POST' or project_settings.DEBUG is True:
-            return_val = True
+        if request.method == 'POST':
+            log.debug( 'method was POST' )
+            if self.check_params( request ) is True:
+                return_val = True
         log.debug( 'return_val, `%s`' % return_val )
         return return_val
 
@@ -32,13 +34,13 @@ class V2_Helper( object ):
         """ Checks params.
             Called by check_validity() """
         log.debug( 'starting check_params()' )
+        log.debug( 'request.POST.keys(), `%s`' % request.POST.keys() )
         return_val = None
-        required_params = [ 'auth_key', 'openurl', 'request_id', 'username' ]
-        for param in required_params:
-            if param is not in request.POST.keys():
+        for param in [ 'auth_key', 'openurl', 'request_id', 'username' ]:
+            log.debug( 'on param, `%s`' % param )
+            if param not in request.POST.keys():
                 return_val = False
                 break
-        if return_val is None:
-            return_val = True
+        return_val = True if ( return_val is None ) else False
         log.debug( 'return_val, `%s`' % return_val )
         return return_val
