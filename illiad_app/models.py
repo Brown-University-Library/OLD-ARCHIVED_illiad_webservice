b2898550f0de9b420956f9135b67a31fb28ff666
birkin <birkin.diana@gmail.com>
2015-10-26 14:45:29 -0400

    adds request-id to log entry.

--- illiad_app/models.py
+++ illiad_app/models.py
@@ -17,7 +17,8 @@ log = logging.getLogger(__name__)
 class V2_Helper( object ):
     """ Handles easyBorrow v2 request. """
 
-    def __init__( self ):
+    def __init__( self, request_id ):
+        self.request_id = request_id
         self.API_KEY = os.environ['ILLIAD_WS__API_AUTH_KEY']
         self.REMOTE_AUTH_URL = os.environ['ILLIAD_WS__REMOTE_AUTH_URL']
         self.REMOTE_AUTH_KEY = os.environ['ILLIAD_WS__REMOTE_AUTH_KEY']  # not the api-key
@@ -26,29 +27,29 @@ class V2_Helper( object ):
     def check_validity( self, request ):
         """ Checks post, auth_key, ip, & params.
             Called by make_request_v2() """
-        log.debug( 'starting check_validity()' )
+        log.debug( '%s - starting check_validity()' % self.request_id )
         return_val = False
         if request.method == 'POST':
             if self.check_params( request ) is True:
                 if request.POST['auth_key'] == self.API_KEY:
                     return_val = True
                 else:
-                    log.debug( 'ip, `%s`' % request.META.get( 'REMOTE_ADDR', 'unavailable' ) )
-        log.debug( 'return_val, `%s`' % return_val )
+                    log.debug( '%s - ip, `%s`' % (self.request_id, request.META.get('REMOTE_ADDR', 'unavailable')) )
+        log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
         return return_val
 
     def check_params( self, request ):
         """ Checks params.
             Called by check_validity() """
-        log.debug( 'starting check_params()' )
+        log.debug( '%s - starting check_params()' % self.request_id )
         return_val = None
         for param in [ 'auth_key', 'openurl', 'request_id', 'username' ]:
-            log.debug( 'on param, `%s`' % param )
+            log.debug( '%s - on param, `%s`' % (self.request_id, param) )
             if param not in request.POST.keys():
                 return_val = False
                 break
         return_val = True if ( return_val is None ) else False
-        log.debug( 'return_val, `%s`' % return_val )
+        log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
         return return_val
 
     def run_request( self, request ):
@@ -59,7 +60,7 @@ class V2_Helper( object ):
         request_key = self._get_request_key( ill, request.POST['openurl'] )
         if request_key['blocked'] is True:
             v2_response_dct = { 'status': 'login_failed_possibly_blocked' }
-            log.debug( 'blocked v2_response_dct, `%s`' % v2_response_dct )
+            log.debug( '%s - blocked v2_response_dct, `%s`' % (self.request_id, v2_response_dct) )
         else:
             submission_response_dct = self._make_request( ill, request_key )
             v2_response_dct = self.build_v2_submitted_response( submission_response_dct )
@@ -70,10 +71,10 @@ class V2_Helper( object ):
             Called by run_request() """
         try:
             ill.login()
-            log.debug( 'illiad remote-auth login successful' )
+            log.debug( '%s - illiad remote-auth login successful' % self.request_id )
             return ill
         except Exception as e:
-            log.error( 'exception, `%s`' % unicode(repr(e)) )
+            log.error( '%s - exception, `%s`' % (self.request_id, unicode(repr(e))) )
             raise Exception( 'Could not log user into ILLiad' )
 
     def _get_request_key( self, ill, openurl ):
@@ -82,10 +83,10 @@ class V2_Helper( object ):
             Called by run_request() """
         try:
             request_key = ill.get_request_key( openurl )
-            log.debug( 'request_key, `%s`' % pprint.pformat(request_key) )
+            log.debug( '%s - request_key, `%s`' % (self.request_id, pprint.pformat(request_key)) )
             return request_key
         except Exception as e:
-            log.error( 'exception, `%s`' % unicode(repr(e)) )
+            log.error( '%s - exception, `%s`' % (self.request_id, unicode(repr(e))) )
             raise Exception( 'Problem submitting openurl to ILLiad' )
 
     def _make_request( self, ill, request_key ):
@@ -93,10 +94,10 @@ class V2_Helper( object ):
             Called by run_request() """
         try:
             submission_response_dct = ill.make_request( request_key )
-            log.debug( 'submission_response_dct, `%s`' % pprint.pformat(submission_response_dct) )
+            log.debug( '%s - submission_response_dct, `%s`' % (self.request_id, pprint.pformat(submission_response_dct)) )
             return submission_response_dct
         except Exception as e:
-            log.error( 'exception, `%s`' % unicode(repr(e)) )
+            log.error( '%s - exception, `%s`' % (self.request_id, unicode(repr(e))) )
             raise Exception( 'Problem submitting request to ILLiad' )
 
     def build_v2_submitted_response( self, submission_response_dct ):
@@ -106,7 +107,7 @@ class V2_Helper( object ):
             v2_response_dct = { 'status': 'submission_successful', 'transaction_number': submission_response_dct['transaction_number'] }
         else:
             v2_response_dct = { 'status': 'submission_failed', 'message': 'see illiad-webservice logs for more info' }
-        log.debug( 'v2_response_dct, `%s`' % pprint.pformat(v2_response_dct) )
+        log.debug( '%s - v2_response_dct, `%s`' % (self.request_id, pprint.pformat(v2_response_dct)) )
         return v2_response_dct
 
     # end class V2_Helper()
--- illiad_app/views.py
+++ illiad_app/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404, render
 from illiad_app.models import V2_Helper
 
 log = logging.getLogger(__name__)
-v2_helper = V2_Helper()
+
 
 
 def info( request ):
@@ -26,6 +26,7 @@ def info( request ):
 def make_request_v2( request ):
     """ Handles current (October 2015) easyBorrow controller illiad call. """
     log.debug( 'starting' )
+    v2_helper = V2_Helper( request.POST.get('request_id', 'no_id') )
     if v2_helper.check_validity( request ) is False:
         return HttpResponseBadRequest( 'Bad Request' )
     v2_response_dct = v2_helper.run_request( request )
