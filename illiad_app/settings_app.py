30a75cc029ec1956b75849fa58474d190d9d407e
birkin <birkin.diana@gmail.com>
2016-06-06 16:50:34 -0400

    updates test of real request.

---
+++ illiad_app/settings_app.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import json, os
+
+
+TEST_AUTH_KEY = os.environ['ILLIAD_WS__API_AUTH_KEY']
+
+TEST_EXISTING_GOOD_USER = os.environ['ILLIAD_WS__TEST_EXISTING_GOOD_USER']
--- illiad_app/tests.py
+++ illiad_app/tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import json
+from . import settings_app
 from django.test import Client, TestCase
 
 
@@ -29,15 +31,17 @@ class ClientV2_Test( TestCase ):
             'Please stop.',
             response.content )
 
-    def test__check_good_post_params( self ):
-        """ Checks POST with good params. """
-        c = Client()
-        response = c.post( '/v2/make_request/', {'auth_key': 'foo_a', 'openurl': 'foo_b', 'request_id': 'foo_c', 'username': 'foo_d'} )
-        self.assertEqual(
-            200,
-            response.status_code )
-        self.assertEqual(
-            True,
-            'request_v2' in response.content )
+    # def test__check_good_post_params__known_user( self ):
+    #     """ POST with good params should submit a request and return a transaction number.
+    #         This test is good, just disabled so as not to submit unnecessary real requests. """
+    #     c = Client()
+    #     response = c.post(
+    #         '/v2/make_request/',
+    #         { 'auth_key': settings_app.TEST_AUTH_KEY, 'openurl': 'foo_b', 'request_id': 'foo_c', 'username': settings_app.TEST_EXISTING_GOOD_USER }
+    #         )
+    #     self.assertEqual( 200, response.status_code )
+    #     response_dct = json.loads( response.content )
+    #     self.assertEqual( [u'status', u'transaction_number'], sorted(response_dct.keys()) )
+    #     self.assertEqual( 'submission_successful', response_dct['status'] )
 
     # end class ClientV2_Test()
