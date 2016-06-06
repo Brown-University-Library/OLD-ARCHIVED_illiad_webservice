d97d08a93f0c4ba17f6443abac9595c685c5feca
birkin <birkin.diana@gmail.com>
2016-06-06 16:48:42 -0400

    removes disabled code.

--- illiad_app/models.py
+++ illiad_app/models.py
@@ -4,12 +4,6 @@ from __future__ import unicode_literals
 import datetime, json, logging, os, pprint
 from illiad.account import IlliadSession
 
-# from django.conf import settings as project_settings
-# from django.core.urlresolvers import reverse
-# from django.db import models
-# from django.http import HttpResponseRedirect
-# from django.utils.encoding import smart_unicode
-# from django.utils.text import slugify
 
 log = logging.getLogger(__name__)
 
